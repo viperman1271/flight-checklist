7242eb25b2867be21585cbc97eb35eed5e97d2fb
Michael Filion <viperman1271@hotmail.com>
2020-12-05 15:18:10 -0500

    Adding CheckListPage

---
+++ source/FlightChecklist/FlightChecklist/UI/ChecklistPage/CheckListPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FlightChecklist
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class CheckListPage : ContentPage
+	{
+		public CheckListPage ()
+		{
+            BindingContext = new ChecklistPageViewModel();
+			InitializeComponent ();
+		}
+	}
+}
